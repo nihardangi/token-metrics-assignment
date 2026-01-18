f00296bb65d9f654c83bedc6ccff177e25e7a2c7
Nihar Dangi <nihardangi@gmail.com>
2026-01-18 23:45:21 +0530

    feat: added comments for mockLockedStrategy contract

--- src/MockLockedStrategy.sol
+++ src/MockLockedStrategy.sol
@@ -4,6 +4,22 @@ pragma solidity ^0.8.24;
 import {ERC4626, ERC20} from "@solmate/tokens/ERC4626.sol";
 import {Ownable} from "@openzeppelin/access/Ownable.sol";
 
+/*
+     * @title MockLockedStrategy
+     * @author Nihar Dangi
+     *
+     * @notice This contract represents a mock strategy that provides locked liquidity for a MultiStrategyVault.
+     * It allows users to deposit and withdraw assets with a lockup period.
+     * Owner can unlock the strategy to allow withdrawals.     
+     *               
+     * Assumptions:     
+     * 1. Strategies are assumed to be vault-specific, meaning they only manage funds from this specific vault.
+     * 2. No fees or performance incentives are implemented in this mock strategy.
+     * 3. Yield generation is simulated by directly transferring assets to the strategy contract.
+     * 4. This mock strategy does not interact with any external protocols; it simply holds the assets.
+     *
+     * @dev Inherits from ERC4626 for vault functionality
+*/
 contract MockLockedStrategy is ERC4626, Ownable {
     bool public unlocked;
 
